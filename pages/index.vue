3ae1cbfb20e54d8121233a17cfafeebbee0be818
alan <jobznnn@163.com>
2021-04-02 17:06:34 +0800

    项目初始化

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,6 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxtjs
-      </h1>
-      <h2 class="subtitle">
-        My astonishing Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <h1>my_blog</h1>
   </div>
 </template>
 
